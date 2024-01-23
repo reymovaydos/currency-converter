ad8fb01282729b35be02e7d2ccbd8c5a49fd1ad9
reymov aydos <reymovaydos83@gmail.com>
2024-01-23 17:08:01 +0500

    currency-conv

---
+++ script.js
@@ -0,0 +1,17 @@
+const uzs = document.querySelector("#uzs"),
+    usd = document.querySelector("#usd");
+
+uzs.addEventListener("input", (e) => {
+    const request = new XMLHttpRequest();
+    request.open("GET", "json/current.json");
+    request.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+    request.send()
+    request.addEventListener("load", () => {
+        if(request.status === 200){
+            const data = JSON.parse(request.response);
+            usd.value = (+uzs.value / data.current.usd).toFixed(2)
+        } else {
+            usd.value = "Something went wrong"
+        }
+    })
+})
